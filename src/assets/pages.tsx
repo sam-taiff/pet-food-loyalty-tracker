cea4fc95d71d741330f1851b9d51c5b03748171a
Josiah Bull <jo@partly.com>
2024-11-16 17:30:10 +1300

    feat: wip changes from josiah laptop.

--- src/assets/pages.tsx
+++ src/assets/pages.tsx
@@ -2,7 +2,7 @@ import '../master.css';
 import { hills } from './brandSVG.tsx';
 import logo from './sparrow.png';
 import { Route, Routes } from 'react-router-dom';
-import { TableComponent } from './view-components.tsx';
+import { TableComponent, VertTable } from './view-components.tsx';
 
 //placeholder text (DEV USE ONLY)
 const lorem: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lectus dui, rutrum sit amet nibh et, consectetur consequat metus. Nunc ultricies enim nec suscipit mollis. Praesent hendrerit, neque nec porta semper, sem tellus venenatis mi, vel sollicitudin tortor elit in libero. Etiam vitae enim eu velit aliquam fringilla. Mauris eleifend ante nisi, sit amet imperdiet purus sodales vitae. Ut posuere rhoncus quam nec dapibus. Proin ullamcorper mauris et lorem dignissim vehicula vitae mattis orci. In eu pulvinar ex. Curabitur euismod tellus quis enim condimentum vehicula. Fusce ac placerat nisi, in ultrices elit. Nulla fringilla ultrices eros, ut dictum felis luctus in. Donec pulvinar tempor felis, sit amet dignissim metus. Maecenas lectus erat, tempor vitae turpis vel, vulputate ultrices nisi."
@@ -97,7 +97,7 @@ export const Brandpage = () => {
     return (
         <>
             <h1>Registered Brands</h1>
-            <TableComponent tableName="Brand" />
+            <VertTable tableName="Brand" />
         </>
     );
 };
